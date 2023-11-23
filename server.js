aa23d15eba69ea6e1c39ae172786befe1d494447
Amina Alibasic <alibasicamina@outlook.com>
2023-11-23 21:18:07 +0100

    Adding routes

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ connectDB();
 // takes a GET req to '/', the res just sends data to the browser
 app.get('/', (req,res) => res.send('API Running'));
 
+// Define routes
+app.use('/api/users', require('./config/routes/api/users'));
+app.use('/api/auth', require('./config/routes/api/auth'));
+app.use('/api/profile', require('./config/routes/api/profile'));
+app.use('/api/posts', require('./config/routes/api/posts'));
+
+
 // this looks for an env variable PORT when the app is deployed, but locally will listen on port 5000
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
