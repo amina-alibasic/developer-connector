e6ff1ba8d9dc61b84a1a444195771047ba2619d5
Amina Alibasic <alibasicamina@outlook.com>
2023-11-23 21:53:50 +0100

    Added additional explanations

--- config/db.js
+++ config/db.js
@@ -1,4 +1,10 @@
 // Connect to MongoDB using Mongoose, with URI from config.js
+/* 
+Moongoose: 
+- provides a way to interact with MongoDB using JavaScript objects 
+- enables performing CRUD operations on MongoDB collections using JavaScript functions
+- provides a querying API that allows constructing complex queries for MongoDB
+*/
 const mongoose = require('mongoose');
 const config = require('config');
 
@@ -7,7 +13,7 @@ const db = config.get('mongoURI');
 // asynchronous arrow function for connecting
 const connectDB = async () => {
     try {
-        await mongoose.connect(db);
+        await mongoose.connect(db); // waits for asynchronous operation (connect) to complete
         console.log('MongoDB Connected!')
     } catch (err) {
         console.error(err.message);
@@ -15,6 +21,8 @@ const connectDB = async () => {
         process.exit(1);
     }
 }
+// The function returns a Promise that resolves with the fetched data
+// which can be handled with .then()
 
 // export the function
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDB from './config/db';
 
+// This here offers a range of functionalities
 const app = express();
 
 // Connect to the DB
@@ -11,6 +12,7 @@ connectDB();
 app.get('/', (req,res) => res.send('API Running'));
 
 // Define routes
+// use is a middleware function used as an entry point for all HTTP requests
 app.use('/api/users', require('./config/routes/api/users').default);
 app.use('/api/auth', require('./config/routes/api/auth').default);
 app.use('/api/profile', require('./config/routes/api/profile').default);
@@ -19,4 +21,5 @@ app.use('/api/posts', require('./config/routes/api/posts').default);
 
 // this looks for an env variable PORT when the app is deployed, but locally will listen on port 5000
 const PORT = process.env.PORT || 5000; 
+// Start the server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
