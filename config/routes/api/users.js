167e421e0175d1faf3674153c433c42e4ad34db5
Amina Alibasic <alibasicamina@outlook.com>
2023-11-24 13:03:52 +0100

    Added prettier and eslint

--- config/routes/api/users.js
+++ config/routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // @route   GET api/users
 // @desc    Test route
@@ -10,10 +11,12 @@ router.get('/', (req,res) => res.send('User route'));
 // @route   POST api/users
 // @desc    Reguster a user
 // @access  public
-router.post('/', (req,res) => {
-    console.log(req.body);  // Object of data that's gonna be sent to this route 
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty()
+], (req,res) => {
+    console.log(req.body);  // Object of data that's gonna be sent to this route, this appears in the console 
     
-    res.send('User route')
+    res.send('User route') // this appears in the browser after sending POST request with Postman
 });
 
 module.exports = router;
